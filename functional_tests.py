8842d169c4b7a67ed62a89739d33814ceeae2978
Dmitry Sankov <dmitry.sankov@gmail.com>
2023-05-05 16:24:43 +0300

    add: FT 02 check ability to save list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import pretty_errors
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     """тест нового посетителя"""
@@ -19,18 +22,36 @@ class NewVisitorTest(unittest.TestCase):
         # списком неотложных дел. Она решает оценить его
         # домашнюю страницу
         self.browser.get('http://localhost:8000')
+
         # Она видит, что заголовок и шапка страницы говорят о
         # списках неотложных дел
         self.assertIn('To-Do', self.browser.title) 
-        self.fail('Закончить тест!') 
-
-
+  
+        header_text = self.browser.find_element(by=By.TAG_NAME, value="h1").text
+        self.assertIn("To-Do", header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox     = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"    
+        )
+        
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
         # вязание рыболовных мушек)
+        inputbox.send_keys("Купить павлинья перья")
+        
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1.5)
+        
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_element(By.TAG_NAME, "tr")
+        self.assertTrue(
+            any(row.text == "1: Купить павлинья перья" for row in rows)
+        )
+        
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
@@ -40,7 +61,8 @@ class NewVisitorTest(unittest.TestCase):
         # выводится небольшой текст с объяснениями.
         # Она посещает этот URL-адрес – ее список по-прежнему там.
         # Удовлетворенная, она снова ложится спать
- 
+        
+        self.fail('Закончить тест!') 
     
 if __name__ == "__main__":
     unittest.main()
