a901d2a5992677c0403011841508d599e6109fd5
Dmitry Sankov <dmitry.sankov@gmail.com>
2023-05-19 18:06:34 +0300

    refactor: new URL view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -8,17 +8,11 @@ def home_page(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         new_item_text = request.POST["item_text"]
         Item.objects.create(text=new_item_text)
-        return redirect("/")
+        return redirect("/lists/the-only-list-in-the-world/")
     items = Item.objects.all()
     return render(request, "home.html", {"items": items})
     
-    # else:
-    #     new_item_text = ""
-        
-    # # item = Item()
-    # # item.text = request.POST.get("item_text", "")
-    # # item.save()
+def view_list(request: HttpRequest) -> HttpResponse:
+    items = Item.objects.all()
+    return render(request, "list.html", {"items": items})
     
-    # return render(request, "home.html", {
-    #     "new_item_text": new_item_text,
-    # })
